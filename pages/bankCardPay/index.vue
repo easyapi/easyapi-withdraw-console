5dd4748c04bb42fd690c57078cc337e6cb9b11fe
leida <zhang20084@126.com>
2021-08-25 10:15:33 +0800

    build:页面整理

---
+++ pages/bankCardPay/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class='container'>
+    <Header></Header>
+    <div :class="showHeader ? 'content' : 'contents'">
+      <Aside></Aside>
+      <div class='main'>
+        <div class='main-title'>
+          <div class='main-title_top'>
+            <b>银行卡设置</b>
+          </div>
+        </div>
+        <div class='main-content mg-tp-20'>
+          <div class='col-bankCardPay-form'>
+            <el-form label-position='left' ref='form' :model='form' label-width='110px'>
+              <el-form-item label='银行卡'>
+                <el-switch
+                  v-model='form.banCardPay'
+                  active-color='#13ce66'>
+                </el-switch>
+              </el-form-item>
+              <el-form-item v-if='form.banCardPay' label='提现方式'>
+                <el-radio v-model='form.radio' label='automaticWithdrawal'>自动提现</el-radio>
+                <el-radio v-model='form.radio' label='manualWithdrawal'>手动提现</el-radio>
+              </el-form-item>
+              <el-form-item v-if="form.banCardPay&&form.radio=='automaticWithdrawal'" label='服务商'>
+                <el-select v-model='form.facilitator'>
+                  <el-option
+                    v-for='item in facilitatorArr'
+                    :key='item.value'
+                    :label='item.label'
+                    :value='item.value'>
+                  </el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item v-if="form.banCardPay&&form.radio=='automaticWithdrawal'">
+                <el-button type='primary'>提交</el-button>
+              </el-form-item>
+            </el-form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Index from './index'
+
+export default Index
+</script>
--- pages/setting/index.vue
+++ pages/setting/index.vue
@@ -10,35 +10,35 @@
           </div>
         </div>
         <div class='main-content mg-tp-20'>
-          <div class="col-setting-form">
-            <el-form label-position="left" ref="form" :model="form" label-width="110px">
-              <el-form-item label="提现结果通知">
+          <div class='col-setting-form'>
+            <el-form label-position='left' ref='form' :model='form' label-width='110px'>
+              <el-form-item label='提现结果通知'>
                 <el-input></el-input>
               </el-form-item>
-              <el-form-item label="">
-                <el-button @click="showMockTest">模拟测试</el-button>
+              <el-form-item label=''>
+                <el-button @click='showMockTest'>模拟测试</el-button>
               </el-form-item>
-              <el-form-item label="提现说明">
+              <el-form-item label='提现说明'>
                 <el-input
-                  type="textarea"
-                  :rows="6"
-                  placeholder="请输入内容">
+                  type='textarea'
+                  :rows='6'
+                  placeholder='请输入内容'>
                 </el-input>
               </el-form-item>
               <el-form-item>
-                <el-button type="primary">提交</el-button>
+                <el-button type='primary'>提交</el-button>
               </el-form-item>
             </el-form>
           </div>
         </div>
       </div>
     </div>
-    <mock-test ref="child"></mock-test>
+    <mock-test ref='child'></mock-test>
   </div>
 </template>
 
 <script>
-  import Index from './index'
+import Index from './index'
 
-  export default Index
+export default Index
 </script>
